aa6fb98cb2b5f0c6a0af7fef8a3506af23847c6a
RITESHSharma-98 <ritesh123.cse@gmail.com>
2026-01-13 01:17:04 +0530

    Added output message for Fibonacci sequence in Hello.java

--- src/Hello.java
+++ src/Hello.java
@@ -2,6 +2,7 @@ class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         Fibonachi fi = new Fibonachi();
+        System.out.println("Fibonacci sequence upto 10 numbers:");
         fi.fibonachi(10);
         System.out.println("Fibonacci sequence until number 100:");
         fi.fibonachiTillMaxNumber(100);
